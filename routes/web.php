03bedd818c4447f5894a983a1384fccf5618c9ed
Anita Baker <anitabaker@myambergroup.com>
2023-08-21 16:35:08 -0500

    fillable properties

--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,12 @@ Route::get('/', [ListingController::class, 'index']);
 // Show Create Form
 Route::get('/listings/create', [ListingController::class, 'create']);
 
+// Store Listing Data
+Route::post('/listings', ListingController::class, 'store');
+
+
+
+
+
 // Single Listing // find by id
 Route::get('/listings/{listing}', ListingController::class, 'show');
